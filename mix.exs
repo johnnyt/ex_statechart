6218b2f3f037d5b00c6990f037bcf7d5394db944
JohnnyT <johnnyt@divvypay.com>
2020-11-11 08:58:59 -0700

    Uses Tesla for requests

--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule StateChart.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :state_chart,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :state_chart,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
@@ -15,9 +17,12 @@ defmodule StateChart.Mixfile do
   end
 
   defp deps do
-    [{:etude, "~> 1.0", only: [:dev, :test]},
-     {:etude_request, "~> 0.2", only: [:dev, :test]},
-     {:poison, ">= 3.0.0", only: [:dev, :test]},
-     {:mix_test_watch, ">= 0.0.0", only: [:dev]}]
+    [
+      {:tesla, "~> 1.3", only: [:dev, :test]},
+      {:etude, "~> 1.0", only: [:dev, :test]},
+      {:etude_request, "~> 0.2", only: [:dev, :test]},
+      {:poison, ">= 3.0.0", only: [:dev, :test]},
+      {:mix_test_watch, ">= 0.0.0", only: [:dev]}
+    ]
   end
 end
--- test/scxml_w3/manifest.exs
+++ test/scxml_w3/manifest.exs
@@ -2,7 +2,7 @@ defmodule Fetch do
   import Record
 
   extract_all(from_lib: "xmerl/include/xmerl.hrl")
-  |> Enum.each(fn({name, kvs}) ->
+  |> Enum.each(fn {name, kvs} ->
     defrecord(name, kvs)
   end)
 
@@ -13,50 +13,52 @@ defmodule Fetch do
     doc
     |> children()
     |> Enum.flat_map(fn
-      (xmlElement(name: :assert) = assert) ->
+      xmlElement(name: :assert) = assert ->
         spec = get_attr(assert, :specid) |> to_string() |> String.replace("#", "")
         [description, test] = children(assert)
+
         description = to_string(xmlText(description, :value))
+
         if get_attr(test, :manual) == 'false' do
           conformance = get_attr(test, :conformance) |> to_string()
+
           test
           |> children()
           |> Stream.map(&get_attr(&1, :uri))
-          |> Stream.filter((&Path.extname(&1) == ".txml"))
-          |> Enum.map(fn(uri) ->
+          |> Stream.filter(&(Path.extname(&1) == ".txml"))
+          |> Enum.map(fn uri ->
             id = Path.basename(uri, ".txml")
             {"#{host}/#{uri}", id, conformance, spec, description}
           end)
         else
           []
         end
-      (xmlText()) ->
+
+      xmlText() ->
         []
     end)
-    |> Enum.map(fn({uri, id, conformance, spec, description}) ->
+    |> Enum.each(fn {uri, id, conformance, spec, description} ->
       name = "#{dir}/#{conformance}/#{spec}/#{id}"
       template = "#{name}.txml"
+
       if !File.exists?(template) do
-        Etude.Request.get(uri)
-        |> Etude.map(fn(%{body: body}) ->
+        with %{"body" => body} <- Tesla.get(uri) do
           name |> Path.dirname() |> File.mkdir_p!()
           File.write!(template, body)
           File.write!("#{name}.description", description)
-          true
-        end)
+        end
       end
     end)
-    |> Etude.join(10)
-    |> Etude.fork!()
 
     File.touch!(touchfile)
   end
 
   defp get_attr(xmlElement(attributes: attrs), name) do
     Enum.find_value(attrs, fn
-      (xmlAttribute(name: ^name, value: value)) ->
+      xmlAttribute(name: ^name, value: value) ->
         value
-      (_) ->
+
+      _ ->
         false
     end)
   end
@@ -64,13 +66,24 @@ defmodule Fetch do
   defp children(xmlElement(content: content)) do
     content
     |> Enum.filter(fn
-      (xmlElement()) -> true
-      (xmlText(type: :cdata)) -> true
-      (_) -> false
+      xmlElement() ->
+        true
+
+      xmlText(type: :text) = xml_text ->
+        xml_text
+        |> xmlText(:value)
+        |> to_string()
+        |> String.trim()
+        |> case do
+          "" -> false
+          _ -> true
+        end
+
+      _ ->
+        false
     end)
   end
 end
 
-Application.ensure_all_started(:etude_request)
-[manifest, touchfile, host] = System.argv
+[manifest, touchfile, host] = System.argv()
 Fetch.run(manifest, touchfile, host)
